c3d79d0e3360cac9a042b7c495db18cd2359b40b
heathercook77 <82852003+heathercook77@users.noreply.github.com>
2021-11-05 17:11:06 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { useState, StrictMode } from "react";
+import { render } from "react-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import Details from "./Details";
+import SearchParams from "./SearchParams";
+import ThemeContext from "./ThemeContext";
+import  "./style.css";
+
+
+const App = () => {
+  const theme = useState("Luxe-Black ");
+  return (
+    <ThemeContext.Provider value={theme}>
+      <div>
+        <Router>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Switch>
+            <Route path="/details/:id">
+              <Details />
+            </Route>
+            <Route path="/">
+              <SearchParams />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </ThemeContext.Provider>
+  );
+};
+export default App;
+
+render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
